c2c65d6060d3f898268b8ab8c1d05a55497a859b
CJ3019853 <CJ3019853@LAB01-PC23>
2023-08-09 12:52:01 -0300

    Adicionar arquivos de projeto.

---
+++ projeto/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbName = new System.Windows.Forms.Label();
+            this.txbEnrollment = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Image = global::projeto.Properties.Resources.c235f11f15dc94534908846de901266f;
+            this.button1.Location = new System.Drawing.Point(34, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 193);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "sobrenome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(294, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // txbName
+            // 
+            this.txbName.AutoSize = true;
+            this.txbName.Location = new System.Drawing.Point(291, 80);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(39, 13);
+            this.txbName.TabIndex = 6;
+            this.txbName.Text = "nome :";
+            this.txbName.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txbEnrollment
+            // 
+            this.txbEnrollment.Location = new System.Drawing.Point(294, 110);
+            this.txbEnrollment.Name = "txbEnrollment";
+            this.txbEnrollment.Size = new System.Drawing.Size(148, 20);
+            this.txbEnrollment.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::projeto.Properties.Resources.papel_de_parede_casual_rosa_bebe_liso_papel_de_parede_android;
+            this.ClientSize = new System.Drawing.Size(733, 610);
+            this.Controls.Add(this.txbEnrollment);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txbName;
+        private System.Windows.Forms.TextBox txbEnrollment;
+    }
+}
+
---
+++ projeto/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = txbName.Text;
+            String enrollment = txbEnrollment.Text;
+            MessageBox.Show(
+                "quem leu ta me devendo 10 de nota de PR2",
+                "sifudeu",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+                               
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}           
